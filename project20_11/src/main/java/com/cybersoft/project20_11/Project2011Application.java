7a9d9573551099991ce7a8976257d357941ae6b7
personman1999 <az0837130202@gmail.com>
2024-11-21 17:06:33 +0700

    dealine20_10

---
+++ project20_11/src/main/java/com/cybersoft/project20_11/Project2011Application.java
@@ -0,0 +1,13 @@
+package com.cybersoft.project20_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2011Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project2011Application.class, args);
+    }
+
+}
---
+++ project20_11/src/main/java/com/cybersoft/project20_11/model/Student.java
@@ -0,0 +1,29 @@
+package com.cybersoft.project20_11.model;
+
+public class Student {
+    private String name;
+    private int age;
+
+    // Constructor
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
