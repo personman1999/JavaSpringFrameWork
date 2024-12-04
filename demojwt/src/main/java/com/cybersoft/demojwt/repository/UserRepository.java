3ce3ffdf348d50855d2d9b5798234715577683b0
personman1999 <az0837130202@gmail.com>
2024-12-04 21:03:11 +0700

    demojwt

---
+++ demojwt/src/main/java/com/cybersoft/demojwt/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.cybersoft.demojwt.repository;
+
+import com.cybersoft.demojwt.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+//    Optional<UserEntity> findByUsername(String username);
+    List<UserEntity> findByUsername(String username);
+}
